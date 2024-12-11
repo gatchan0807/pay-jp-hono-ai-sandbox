8ee88758eeb56795750c3048ddce72c4bd2d6229
gatchan0807 <yuki.dolphin.koga@gmail.com>
2024-12-11 23:41:02 +0900

    feat: トークン化ができたら顧客情報を作るところまで一旦ごちゃっとRouter上に実装

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,9 @@
 import { Hono } from 'hono'
+import { env } from 'hono/adapter'
+
 import { renderer } from './renderer'
 import { TopPage } from './page/top'
+import { CustomerResponse, ErrorResponse } from './types/payjp'
 
 const app = new Hono()
 
@@ -10,14 +13,47 @@ app.get('/', (c) => {
   return c.render(<TopPage />)
 })
 
-app.post('/pay', async (c) => {
+app.post('/payment', async (c) => {
   const body = await c.req.parseBody()
   const token = body["payjp-token"] // PAY.JPから提供される JS でできるUIのPOST時のデフォルト値
-  return c.render(`Payment completed! ${token}`)
-})
+  if (!token) {
+    return c.render("Token not found")
+  }
 
-app.get('/hello/:name', (c) => {
-  return c.text(`Hello ${c.req.param("name")}!`)
+  if (typeof token !== "string") {
+    return c.render("Token is not string")
+  }
+
+  const { PAYJP_SECRET_KEY } = env<{ PAYJP_SECRET_KEY: string }>(c)
+  if (PAYJP_SECRET_KEY === "") {
+    return c.render("PAYJP_SECRET_KEY is not found")
+  }
+
+  const encodedCredentials = Buffer.from(`${PAYJP_SECRET_KEY}:`).toString("base64")
+
+  const params = new URLSearchParams({
+    card: token,
+  });
+  const formData = params.toString(); 
+
+  const customer = await fetch("https://api.pay.jp/v1/customers", {
+    method: "POST",
+    
+    headers: {
+      Authorization: `Basic ${encodedCredentials}:`,
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
+    body: formData,
+  })
+  const customerJson = await customer.json<CustomerResponse | ErrorResponse>()
+  
+  console.log(customerJson)
+
+  if ("error" in customerJson) {
+    return c.render(`Error: ${customerJson.error.message}`)
+  }
+
+  return c.render(`Customer Created! ${JSON.stringify(customerJson.id)}`)
 })
 
 export default app
--- src/page/top.tsx
+++ src/page/top.tsx
@@ -8,7 +8,7 @@ export const TopPage:FC = () => {
         <p>
           PAY.JPで支払いを行う（テストカードしか使えません）
           <p>テストカード番号：4242 4242 4242 4242</p>
-          <form action="/pay" method="post">
+          <form action="/payment" method="post">
             <script src="https://checkout.pay.jp" class="payjp-button" data-key="pk_test_52f40932ba5a099b40ed9974"></script>
           </form>
         </p>
