ad359103d598331c35911397f4283c6728c10af1
gatchan0807 <yuki.dolphin.koga@gmail.com>
2024-12-11 22:27:10 +0900

    feat: PAY.JPから提供されているUIを使ってカード情報をトークン化する処理を追加

--- src/index.tsx
+++ src/index.tsx
@@ -10,6 +10,12 @@ app.get('/', (c) => {
   return c.render(<TopPage />)
 })
 
+app.post('/pay', async (c) => {
+  const body = await c.req.parseBody()
+  const token = body["payjp-token"] // PAY.JPから提供される JS でできるUIのPOST時のデフォルト値
+  return c.render(`Payment completed! ${token}`)
+})
+
 app.get('/hello/:name', (c) => {
   return c.text(`Hello ${c.req.param("name")}!`)
 })
