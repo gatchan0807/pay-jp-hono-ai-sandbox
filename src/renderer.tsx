bf489ef35a6efad63999aba8ddf9a305af23869b
gatchan0807 <yuki.dolphin.koga@gmail.com>
2024-12-14 15:05:48 +0900

    feat: TailwindCSSを全面的にCDN経由で使うように変更

--- src/renderer.tsx
+++ src/renderer.tsx
@@ -4,7 +4,7 @@ export const renderer = jsxRenderer(({ children }) => {
   return (
     <html>
       <head>
-        <link href="/static/style.css" rel="stylesheet" />
+        <script src="https://cdn.tailwindcss.com"></script>
       </head>
       <body>{children}</body>
     </html>
