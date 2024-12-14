5a222eb6b1d7bb9af54164f4b13b9f13244664b2
gatchan0807 <yuki.dolphin.koga@gmail.com>
2024-12-14 15:37:51 +0900

    chore: スタイルを当てるために共通コンポーネントを作成

---
+++ src/components/box.tsx
@@ -0,0 +1,7 @@
+export function Container({ children }: { children?: any }) {
+    return (
+        <div class="mx-8">
+            {children}
+        </div>
+    )
+}
---
+++ src/components/text.tsx
@@ -0,0 +1,5 @@
+export function Title({ text }: { text: string }) {
+    return (
+        <h1 class="font-bold text-xl py-4">{text}</h1>
+    )
+}
