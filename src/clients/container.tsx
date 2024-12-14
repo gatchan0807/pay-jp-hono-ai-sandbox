dd0922277d91852da589de71b5bcb7e5d221f5bb
gatchan0807 <yuki.dolphin.koga@gmail.com>
2024-12-14 15:06:09 +0900

    feat: クライアントサイドレンダリングでAIモデルが使えるのを確認できるようにした

--- src/clients/container.tsx
+++ src/clients/container.tsx
@@ -1,9 +1,15 @@
 import { render } from 'hono/jsx/dom'
+import { useLanguageModelStatus } from './hooks/useLanguageModelStatus'
+import { useLanguageModel } from './hooks/useLanguageModels';
 import { Footer } from '../components/footer';
 
 export function Container() {
+  const languageModel = useLanguageModel();
+  const languageModelStatus = useLanguageModelStatus({ languageModel });
+
   return <>
     <h1>Hello world from Client side rendering!</h1>
+    <h1>Can I use on-chrome LLM?: {JSON.stringify(languageModelStatus)}</h1>
 
     <Footer />
   </>
