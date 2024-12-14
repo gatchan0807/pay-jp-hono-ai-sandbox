8c44045d3b9644969957753887536e90fca2a6e8
gatchan0807 <yuki.dolphin.koga@gmail.com>
2024-12-14 15:17:52 +0900

    fix: クライアントサイドのビルドが失敗していたので修正

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,30 @@
-import build from '@hono/vite-build/cloudflare-pages'
+import pages from '@hono/vite-cloudflare-pages'
 import devServer from '@hono/vite-dev-server'
-import adapter from '@hono/vite-dev-server/cloudflare'
 import { defineConfig } from 'vite'
 
-export default defineConfig({
-  plugins: [
-    build(),
-    devServer({
-      adapter,
-      entry: 'src/index.tsx'
-    })
-  ]
+export default defineConfig(({ mode }) => {
+  if (mode === 'client') {
+    return {
+      esbuild: {
+        jsxImportSource: 'hono/jsx/dom' // Optimized for hono/jsx/dom
+      },
+      build: {
+        rollupOptions: {
+          input: './src/clients/container.tsx',
+          output: {
+            entryFileNames: 'static/client.js'
+          }
+        }
+      }
+    }
+  } else {
+    return {
+      plugins: [
+        pages(),
+        devServer({
+          entry: 'src/index.tsx'
+        })
+      ]
+    }
+  }
 })
