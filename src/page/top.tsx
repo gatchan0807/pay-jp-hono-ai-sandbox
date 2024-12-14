4e3b2155edf32b12fb38b1274bc20225c3d9df33
gatchan0807 <yuki.dolphin.koga@gmail.com>
2024-12-14 15:04:53 +0900

    feat: フォーマットをかけてFooterコンポーネントをこっちにも適用

--- src/page/top.tsx
+++ src/page/top.tsx
@@ -1,17 +1,20 @@
 import { FC, Suspense } from "hono/jsx";
+import { Footer } from "../components/footer";
 
-export const TopPage:FC = () => {
-    return (
-      <Suspense fallback={<div>Loading...</div>}>
-        <h1>Hello World from Cloudflare Pages!</h1>
+export const TopPage: FC = () => {
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <h1>Hello World from Cloudflare Pages!</h1>
 
-        <p>
-          PAY.JPで支払いを行う（テストカードしか使えません）
-          <p>テストカード番号：4242 4242 4242 4242</p>
-          <form action="/payment" method="post">
-            <script src="https://checkout.pay.jp" class="payjp-button" data-key="pk_test_52f40932ba5a099b40ed9974"></script>
-          </form>
-        </p>
-      </Suspense>
-    )
-  }
+      <p>
+        PAY.JPで支払いを行う（テストカードしか使えません）
+        <p>テストカード番号：4242 4242 4242 4242</p>
+        <form action="/payment" method="post">
+          <script src="https://checkout.pay.jp" class="payjp-button" data-key="pk_test_52f40932ba5a099b40ed9974"></script>
+        </form>
+      </p>
+
+      <Footer />
+    </Suspense>
+  )
+}
