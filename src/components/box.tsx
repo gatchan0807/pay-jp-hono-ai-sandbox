9157dc2b956626b4c02ec2275418128120adb0ac
gatchan0807 <yuki.dolphin.koga@gmail.com>
2024-12-15 14:24:15 +0900

    feat: 注釈ボックスに成功時の色を追加

--- src/components/box.tsx
+++ src/components/box.tsx
@@ -6,9 +6,9 @@ export function Container({ children }: { children?: any }) {
     )
 }
 
-export function AttentionBox({ type, children }: { type: "info" | "warn" | "error", children?: any }) {
+export function AttentionBox({ type, children }: { type: "info" | "warn" | "error" | "success", children?: any }) {
     return (
-        <div class={`p-4 my-2 rounded ${type === "info" ? "bg-blue-100 text-blue-600" : type === "warn" ? "bg-yellow-100 text-yellow-600" : "bg-red-100 text-red-600"}`}>
+        <div class={`p-4 my-2 rounded ${type === "info" ? "bg-blue-100 text-blue-600" : type === "warn" ? "bg-yellow-100 text-yellow-600" : type === "error" ? "bg-red-100 text-red-600" : "bg-green-100 text-green-600"}`}>
             {children}
         </div>
     )
