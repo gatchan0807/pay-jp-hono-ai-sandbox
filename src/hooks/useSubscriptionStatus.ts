2816356e2f9a7cde2c2f2264e748e4b523acafce
gatchan0807 <yuki.dolphin.koga@gmail.com>
2024-12-15 14:27:36 +0900

    feat: サブスク登録済みの場合はプラン情報を表示するようにする

---
+++ src/hooks/useSubscriptionStatus.ts
@@ -0,0 +1,21 @@
+import { Cookie } from "../page/top"
+import { getSubscription } from "./fetchPayJp"
+
+
+export async function useSubscriptionStatus(
+    cookie: Cookie, credentials: string) {
+    if (!cookie.customerId || !cookie.subscriptionId || !cookie.planId) {
+        return { status: 'guest', plan: null }
+    }
+
+    const { subscription, error } = await getSubscription(credentials, cookie.subscriptionId)
+    if (error) {
+        return { status: 'inactive', plan: null }
+    }
+
+    if ("error" in subscription) {
+        return { status: 'inactive', plan: null }
+    }
+
+    return { status: 'active', plan: subscription.plan }
+}
--- src/page/top.tsx
+++ src/page/top.tsx
@@ -1,9 +1,19 @@
-import { FC, Suspense } from "hono/jsx";
+import { Suspense } from "hono/jsx";
 import { Footer } from "../components/footer";
 import { Title } from "../components/text";
 import { AttentionBox, Container } from "../components/box";
+import { useSubscriptionStatus } from "../hooks/useSubscriptionStatus";
+import { PlanResponse } from "../types/payjp";
 
-export const TopPage: FC = () => {
+export type Cookie = {
+  customerId: string | false
+  subscriptionId: string | false
+  planId: string | false
+}
+
+export async function TopPage({ cookie, credentials }: { cookie: Cookie, credentials: string }) {
+  const { status: subscriptionStatus, plan } = await useSubscriptionStatus(cookie, credentials)
+
   return (
     <Suspense fallback={<div>Loading...</div>}>
       <Container>
@@ -13,16 +23,45 @@ export const TopPage: FC = () => {
           <>PAY.JP API経由で顧客情報とサブスク登録、テストカードの登録までやってみよう！</>
         </AttentionBox>
 
-        <p>
-          PAY.JPで支払いを行う（テストカードしか使えません）
-          <p>テストカード番号：4242 4242 4242 4242</p>
-          <form action="/payment" method="post">
-            <script src="https://checkout.pay.jp" class="payjp-button" data-key="pk_test_52f40932ba5a099b40ed9974"></script>
-          </form>
-        </p>
+        {subscriptionStatus === 'active' && plan && (
+          <Plan plan={plan} />
+        )}
+        {subscriptionStatus !== 'active' && (
+          <PayJpSubscriptionForm />
+        )}
 
         <Footer />
       </Container>
     </Suspense>
+  )
+}
+
+function Plan({ plan }: { plan: PlanResponse }) {
+  return (
+    <div class="my-4 p-4 bg-slate-100 text-slate-600">
+      <ul>
+        <p>現在のサブスクリプション情報</p>
+        <li>プラン: {plan.name}</li>
+        <li>金額: {plan.amount}円</li>
+        <li>次回支払日: {plan.billing_day}日</li>
+        <li>トライアル期間: {plan.trial_days}日</li>
+      </ul>
+
+      <div class="w-full flex justify-end">
+        <button class="mr-4 mt-4 px-4 py-2 bg-red-100 text-red-800 rounded">解約する</button>
+      </div>
+    </div>
+  )
+}
+
+function PayJpSubscriptionForm() {
+  return (
+    <p>
+      PAY.JPで支払いを行う（テストカードしか使えません）
+      <p>テストカード番号：4242 4242 4242 4242</p>
+      <form action="/payment" method="post">
+        <script src="https://checkout.pay.jp" class="payjp-button" data-key="pk_test_52f40932ba5a099b40ed9974"></script>
+      </form>
+    </p>
   )
 }
