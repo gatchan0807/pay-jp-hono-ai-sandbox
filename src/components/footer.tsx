4754e2a63e5035cf9ab6a588eddc61b19f634ddc
gatchan0807 <yuki.dolphin.koga@gmail.com>
2024-12-17 12:29:08 +0900

    feat: footer周りのスタイル調整

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,12 +1,16 @@
 export function Footer() {
     return (
-        <footer>
-            <ul class="pl-8 py-4 mt-4 w-full bg-slate-100 list-disc">
+        <footer class="pl-8 py-4 w-full bg-slate-100">
+            <p>ページ一覧</p>
+            <ul >
                 <li>
                     <a href="/" class="underline text-teal-600 hover:text-teal-500">TOP</a>
                 </li>
                 <li>
-                    <a href="/ai/limited" class="underline text-teal-600 hover:text-teal-500">AI</a>
+                    <a href="/ai/limited" class="underline text-teal-600 hover:text-teal-500">機能限定版AI</a>
+                </li>
+                <li>
+                    <a href="/ai/premium" class="underline text-teal-600 hover:text-teal-500">プレミアム版AI</a>
                 </li>
             </ul>
         </footer>
