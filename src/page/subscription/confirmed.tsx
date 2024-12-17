24986119b84edfa1fa7fa57bae7681c3f3923977
gatchan0807 <yuki.dolphin.koga@gmail.com>
2024-12-17 12:30:13 +0900

    feat: サブスク登録時にプレミアムプランの画面を見えるようにリンクを貼る

--- src/page/subscription/confirmed.tsx
+++ src/page/subscription/confirmed.tsx
@@ -5,10 +5,10 @@ import { SubscriptionResponse } from "../../types/payjp"
 
 export function ConfirmedPage({ subscription }: { subscription: SubscriptionResponse }) {
     return (
-        <Container>
+        <Container gap={4}>
             <Title text="サブスクリプション登録完了" />
             <h2 class="text-xl">サブスクリプション登録が完了しました！（テストカードなので、実際には決済されません）</h2>
-            <div class="my-4 p-4 bg-slate-100 text-slate-600">
+            <div class="p-4 bg-slate-100 text-slate-600">
                 <ul>
                     <p>サブスクリプションID: {subscription.id}</p>
                     <li>プラン: {subscription.plan.name}</li>
@@ -17,7 +17,9 @@ export function ConfirmedPage({ subscription }: { subscription: SubscriptionResp
                     <li>次回支払い日（トライアル終了日）: {new Date((subscription.trial_end ?? 0) * 1000).toLocaleDateString('ja-JP')}</li>
                 </ul>
             </div>
-            <a href="/ai/premium"></a>
+            <div class="w-full flex justify-center">
+                <a href="/ai/premium" class="block p-4 w-1/ text-center bg-teal-200 hover:bg-teal-100 text-teal-600 rounded">プレミアム版AIを使ってみる</a>
+            </div>
             <Footer />
         </Container>
     )
