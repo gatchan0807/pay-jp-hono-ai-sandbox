4ad2d63b083f8cf93e3fc172a9a6a64c29856ed2
gatchan0807 <yuki.dolphin.koga@gmail.com>
2025-01-01 12:35:36 +0900

    chore: run fmt

--- src/routes/ai/limited.tsx
+++ src/routes/ai/limited.tsx
@@ -4,4 +4,4 @@ import { RenderingClientComponent } from "../../csr/utils/client-component"
 
 export const AiLimitedHandler = (ctx: Context<BlankEnv, string, BlankInput>) => {
     return RenderingClientComponent(ctx, { dev: "/src/csr/pages/ai.tsx", prd: "/static/ai/limited.js" })
-}      
+}
